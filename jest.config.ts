1a0984d56490f52a86c74deae91e1e92248fa35b
Camille Fontaine <camille.fontaine93@gmail.com>
2025-08-19 20:18:10 -0700

    lkj

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,14 @@
 import { pathsToModuleNameMapper } from "ts-jest";
-const tsConfig = require('./tsconfig.test.json');
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const tsConfig = JSON.parse(
+  readFileSync(join(__dirname, 'tsconfig.test.json'), 'utf8')
+);
 
 export default {
   preset: "ts-jest",
