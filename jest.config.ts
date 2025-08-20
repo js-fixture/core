537c6a22d7397e8301df5bd854d56b40ed8ac797
Camille Fontaine <100146576+camille-fontaine1@users.noreply.github.com>
2025-08-19 20:00:10 -0700

    ci: add a unit test workflow

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,20 @@
 import { pathsToModuleNameMapper } from "ts-jest";
-import { compilerOptions } from "./tsconfig.test.json";
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const tsConfig = JSON.parse(
+  readFileSync(join(__dirname, 'tsconfig.test.json'), 'utf8')
+);
 
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
   moduleDirectories: ["node_modules", "<rootDir>"],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
+  moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths, { prefix: "<rootDir>/" }),
   roots: ["<rootDir>/tests"],
   testMatch: ["**/*.test.ts"],
   transform: {
